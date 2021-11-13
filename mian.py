5558df3d97c2b76123ca6a815176398e7146da4b
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-11-12 23:57:36 -0500

    Simple GUI template added

--- mian.py
+++ mian.py
@@ -5,12 +5,8 @@ import cv2
 import pytesseract
 
 root = tk.Tk()
-root.withdraw()
-style = ttk.Style(root)
-style.theme_use("clam")
 
-
-def get_Image():
+def get_Image(tk_text):
     # Browse local files & get path
     file_path = filedialog.askopenfilename(parent=root, initialdir='/', initialfile='tmp',
                                            filetypes=[("All files", "*")])
@@ -18,8 +14,36 @@ def get_Image():
     # Convert image to text with tesseract
     img = cv2.imread(file_path)
     text = pytesseract.image_to_string(img, lang="eng")
-    print(text)
+    tk_text.insert(tk.END, text)
+
+
+def start_UI():
+    # GUI
+    root.geometry('300x500')
+    root.resizable(False, False)
+    root.title('Image2Text')
+    var = tk.StringVar()
+    welcome_msg = "Welcome! " \
+                  "\n\nExtract text from image -> download as .txt file." \
+                  "\n-TO START CLICK ON BROWSE- "
+    var.set(welcome_msg)
+    title = tk.Label(root, textvariable=var)
+    title.pack()
+    # Text box location to set the extracted text
+    text_entry = tk.Text(root, width=30, height=25)
+    text_entry.place(relx=0.5, rely=1.1, anchor=tk.S)
+    # Buttons
+    browse_button = tk.Button(root, text='Browse', command=lambda: get_Image(text_entry))
+    browse_button.pack(side=tk.LEFT, ipadx=5, ipady=5, expand=True)
+    browse_button.place(relx=0.3, rely=0.2, anchor=tk.CENTER)
+
+    exit_button = tk.Button(root, text="Exit", fg="red", command=lambda: root.quit())
+    exit_button.pack(side=tk.RIGHT, ipadx=5, ipady=5, expand=True)
+    exit_button.place(relx=0.7, rely=0.2, anchor=tk.CENTER)
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    get_Image()
+    start_UI()
+
+
